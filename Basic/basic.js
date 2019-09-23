eac3ac1e7d6d10cc0dda6a759b9af5ffa84a15e4
Robert Flunger <robert.flunger@gmx.at>
2019-09-23 10:26:45 +0200

    Added Exercise 3

--- Basic/basic.js
+++ Basic/basic.js
@@ -1,5 +1,5 @@
 function crystalGazer(a,b,c,d) {
-	var sentence = "You will be a " + a + " in " + b + " and married to " + c + " with " + d +".";
+	var sentence = "You will be a " + a + " in " + b + " and married to " + c + " with " + d +"." +"<br>";
 	console.log(sentence);
 	return sentence;
 }
@@ -8,10 +8,23 @@ document.write(crystalGazer("dentist", "California", "a wonderful woman", 3))
 
 function ageCalculator(x,y) {
 	var age = y-x;
+	var theAge = " you are either " + age + " or " + --age + "<br>";
+	console.log(age);
+	
+	return theAge;
+}
+	document.write(ageCalculator(1990, 2019))
+
+function ageCalculator2(x) {
+	var d = new Date();
+	var year = d.getFullYear()
+	
+	var age = year - x;
+	
 	var theAge = " you are either " + age + " or " + --age;
 	console.log(age);
 	
 	return theAge;
 }
-	document.write(ageCalculator(1990, 2018))
+	document.write(ageCalculator2(1990))
 
