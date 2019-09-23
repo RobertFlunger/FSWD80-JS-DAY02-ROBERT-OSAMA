d777dc3cf4aa0e860b04c570c472c23859d96351
alhussinosama <osamahossien@yahoo.com>
2019-09-23 10:12:55 +0200

    add file

--- Basic/basic.js
+++ Basic/basic.js
@@ -5,3 +5,13 @@ function crystalGazer(a,b,c,d) {
 }
 
 document.write(crystalGazer("dentist", "California", "a wonderful woman", 3))
+
+function ageCalculator(x,y) {
+	var age = y-x;
+	var theAge = " you are either " + age + " or " + --age;
+	console.log(age);
+	
+	return theAge;
+}
+	document.write(ageCalculator(1990, 2018))
+
