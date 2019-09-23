65d3c4ad94e89e05f2ac1fd0228a5fa266250e23
Robert Flunger <robert.flunger@gmx.at>
2019-09-23 09:52:34 +0200

    Created Basic Exercise 01

---
+++ Basic/basic.js
@@ -0,0 +1,7 @@
+function crystalGazer(a,b,c,d) {
+	var sentence = "You will be a " + a + " in " + b + " and married to " + c + " with " + d +".";
+	console.log(sentence);
+	return sentence;
+}
+
+document.write(crystalGazer("dentist", "California", "a wonderful woman", 3))
