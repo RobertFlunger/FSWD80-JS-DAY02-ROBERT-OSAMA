2dbe31a23c1db8f5fb6c40f18308773fe117301a
Robert Flunger <robert.flunger@gmx.at>
2019-09-23 11:25:11 +0200

    bla

--- Intermediate/intermediate.js
+++
@@ -1,8 +0,0 @@
-/*Intermediate Exercise 1*/
-
-function capitalizeFirst(x) {
-	var upper = x.charAt(0).toUpperCase() + x.substring(1);
-	return(upper);
-}
-
-	document.write(capitalizeFirst("i am a web developer"));
