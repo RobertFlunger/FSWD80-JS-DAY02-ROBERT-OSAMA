90fe7a4eebaab9e7b02af8516726c974ae504981
alhussinosama <osamahossien@yahoo.com>
2019-09-23 14:36:38 +0200

    file average prompt

--- average prompt/average.js
+++ average prompt/average.js
@@ -3,9 +3,9 @@
 
 	function grade() {
 
-		var a= prompt(" please inter a math grade");
-		var	b= prompt("please inter a phisic grade");
-		var	c= prompt("please inter english grade");
+		var a= prompt("please enter a math grade");
+		var	b= prompt("please enter a physics grade");
+		var	c= prompt("please enter an english grade");
 		var	sum= Number(a)+ Number(b) + Number(c);
 		var average= sum /3;
 		var stringgrade= " the sum is: " + sum + " <br>" + " the average is : "+ average ;
